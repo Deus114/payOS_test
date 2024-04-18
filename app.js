d1ef80f249e40e1b21d3862406d03801005de679
DESKTOP-24GU9HH\ASUS <lekhaduy0917880045@gmail.com>
2024-04-18 18:37:42 +0700

    test connect

--- app.js
+++ app.js
@@ -3,28 +3,41 @@ require('dotenv').config();
 const express = require('express');
 const PayOS = require('@payos/node');
 
-const payos = new PayOS('client-id', 'api-key', 'checksum-key');
+const payos = new PayOS(
+    '0082e1ad-0c99-44d9-a50d-e09329b6a0fb',
+    '322cef93-0491-46a4-baa2-745d5a975b7a',
+    '608d75517b129265ef1f4f4f452de476b501a1b5af7e518644d489dda6f96f76'
+);
 
-const DOMAIN = 'http://localhost:3000';
+const DOMAIN = 'http://localhost:3001';
 const port = process.env.PORT || 4000;
 const hostname = process.env.HOST_NAME;
 const app = express();
+const connection = require('./src/configdb')
+const mongoose = require('mongoose')
 
 app.use(express.static('src'));
 app.use(express.json());
 
 app.post('/create-payment-link', async (req, res) => {
     const order = {
-        amount: 1,
+        amount: 10000,
         description: 'Thanh toan mua sach',
-        ordercode: 1,
+        orderCode: 13,
         returnUrl: `${DOMAIN}/success.html`,
         cancelUrl: `${DOMAIN}/cancel.html`,
     };
-    const paymentLink = await payos.createPaymentLink(oder);
+    const paymentLink = await payos.createPaymentLink(order);
     res.redirect(303, paymentLink.checkoutUrl);
 });
 
-app.listen(port, hostname, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+(async () => {
+    try {
+        await connection();
+        app.listen(port, hostname, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+    } catch (error) {
+        console.log('>>> DB conect error: ', error);
+    }
+})()
